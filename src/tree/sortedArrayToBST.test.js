bf2d8a57a92b1162bf82a483d5ed304f600f7465
adam <ch512006634@gmail.com>
2022-05-04 12:19:41 +0800

    有序数组转高度平衡的二叉搜索树

---
+++ src/tree/sortedArrayToBST.test.js
@@ -0,0 +1,24 @@
+/**
+ * 有序数组转高度平衡的二叉搜索树
+ */
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function sortedArrayToBST(nums) {
+  function helper(nums, lower, upper) {
+    if (lower > upper) {
+      return;
+    }
+    let mid = (upper - lower) / 2;
+    let root = new Node(nums[mid]);
+    root.left = helper(nums, lower, mid - 1);
+    root.right = helper(nums, mid + 1, upper);
+  }
+  return helper(nums, 0, nums.length - 1);
+}
