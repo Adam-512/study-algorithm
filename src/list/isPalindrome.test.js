81448eda0b9bd635d997111c877f947b789bc8e9
adam <ch512006634@gmail.com>
2022-05-05 10:50:00 +0800

    测试不通过修复

--- src/list/isPalindrome.test.js
+++ src/list/isPalindrome.test.js
@@ -2,42 +2,46 @@
  * 回文链表
  */
 
-function reverse(node){
-  let cur = node
-  let prev = null
-  while(cur!=null){
-    const next = cur.next
-    cur.next = prev
-    prev = cur
-    cur = next
+ const reverseList = (head) => {
+  let prev = null;
+  let curr = head;
+  while (curr !== null) {
+      let nextTemp = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = nextTemp;
   }
-  return prev
+  return prev;
 }
 
-function getHalf(node) {
-  let fast = node
-  let slow = node
-  if (fast.next != null && fast.next.next != null) {
-    fast = fast.next.next
-    slow = slow.next
+const endOfFirstHalf = (head) => {
+  let fast = head;
+  let slow = head;
+  while (fast.next !== null && fast.next.next !== null) {
+      fast = fast.next.next;
+      slow = slow.next;
   }
-  return slow
+  return slow;
 }
 
+var isPalindrome = function(head) {
+  if (head == null) return true;
 
-function isPalindrome(head) {
-  const firstHalf = getHalf(head)
-  const seccondHalf = reverse(firstHalf.next)
+  // 找到前半部分链表的尾节点并反转后半部分链表
+  const firstHalfEnd = endOfFirstHalf(head);
+  const secondHalfStart = reverseList(firstHalfEnd.next);
 
-  let result = true
-  let p1 = firstHalf
-  let p2 = seccondHalf
-  while (result && p1 != null) {
-    if (p1.val != p2.val) result = false
-    p1 = p1.next
-    p2.next = p2.next
-  }
+  // 判断是否回文
+  let p1 = head;
+  let p2 = secondHalfStart;
+  let result = true;
+  while (result && p2 != null) {
+      if (p1.val != p2.val) result = false;
+      p1 = p1.next;
+      p2 = p2.next;
+  }        
 
-  firstHalf.next = reverse(seccondHalf)
-  return result
-}
+  // 还原链表并返回结果
+  firstHalfEnd.next = reverseList(secondHalfStart);
+  return result;
+};
